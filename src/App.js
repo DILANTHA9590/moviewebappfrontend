d4f5a70a087c130617b917e912a7840947a0f175
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-09 08:21:37 +0530

    Set up React Router with HomePage route and toast notifications🌟✅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
-import Home from "./home";
+
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <BrowserRouter>
+        <Toaster />
+        <Routes>
+          <Route path="/*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
