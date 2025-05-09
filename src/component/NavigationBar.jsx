1d47e62ed59ac93216871de20ca0d70c2b67397e
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-09 08:18:24 +0530

    create nav bar🌟

---
+++ src/component/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function NavigationBar() {
+  return (
+    <>
+      <div className="h-[10vh] bg-black">
+        <div>
+          <div>
+            <h1>logo sectin</h1>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
